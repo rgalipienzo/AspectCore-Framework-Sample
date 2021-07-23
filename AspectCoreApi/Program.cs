de9b900a8157a4132a27fc9fb62100e5ad593553
Ricardo Galipienzo <rgalipienzo@gmail.com>
2021-07-24 00:36:01 +0200

    added Serilog

--- AspectCoreApi/Program.cs
+++ AspectCoreApi/Program.cs
@@ -1,6 +1,7 @@
 using AspectCore.Extensions.Hosting;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace AspectCoreApi
 {
@@ -8,11 +9,17 @@ namespace AspectCoreApi
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
